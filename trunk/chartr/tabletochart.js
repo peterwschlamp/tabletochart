1b3dfa1871f7419dba03341906dc212640fa87d9
peterwschlamp@gmail.com <peterwschlamp@gmail.com@57320a5a-23a0-820b-e713-0890668e8b8c>
2011-06-08 20:34:47 +0000

    just committing

--- trunk/chartr/tabletochart.js
+++ trunk/chartr/tabletochart.js
@@ -1,7 +1,7 @@
       /*
 	  
 // TableToChart by Peter Schlamp //
-// peterwschlamp@gmail.com //
+// peterwschlamp@gmail.com /////
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
